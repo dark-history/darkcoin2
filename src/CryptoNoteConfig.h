11f3174372845f2acd8ca19058c6ac545026a476
cash2 <cash2@protonmail.com>
2018-10-27 13:52:15 -0400

    Updated Copyright
    Included Copyright (c) 2018 The Cash2 developers at the top of each page

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -1,4 +1,5 @@
 // Copyright (c) 2011-2016 The Cryptonote developers
+// Copyright (c) 2018 The Cash2 developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -10,62 +11,40 @@
 namespace CryptoNote {
 namespace parameters {
 
-//TODO Define preferred block's target time
 const uint64_t DIFFICULTY_TARGET                             = 9; // seconds
-
-const uint64_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = ((uint64_t)(-1));
+const uint64_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = ((uint64_t)(-1)); // 18,446,744,073,709,551,616 blocks
 const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                = 500000000;
 const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 1000000000;
-//TODO Currency-specific address prefix
-const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 0x6;
-//TODO Choose maturity period for your currency
-const size_t   CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW          = 100;
+const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 0x6; // all Cash2 addresses start with a '2' 
+const size_t   CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW          = 60;
 const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 3 * DIFFICULTY_TARGET;
-
 const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 11;
-
-//TODO Specify total number of available coins
-//TODO ((uint64_t)(-1)) equals to 18446744073709551616 coins
-//TODO or you can define number explicitly UINT64_C(858986905600000000)
-const uint64_t MONEY_SUPPLY                                  = UINT64_C(15000000000000000);
+const uint64_t MONEY_SUPPLY                                  = UINT64_C(15000000000000000); // 15,000,000.000000000 total coins
 const unsigned EMISSION_SPEED_FACTOR                         = 24;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
-
-//TODO Define number of blocks for block size median calculation
 const size_t   CRYPTONOTE_REWARD_BLOCKS_WINDOW               = 60 * 24 * 60 * 60 / DIFFICULTY_TARGET; // number of blocks produced in 60 days
-// size of block (bytes) after which reward for block calculated using block size
 const size_t   CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE     = 1024 * 1024; //size of block (bytes) after which reward for block calculated using block size, 1 mb
 const size_t   CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE        = 600;
-//TODO Define number of digits
-const size_t   CRYPTONOTE_DISPLAY_DECIMAL_POINT              = 9;
-//TODO Define minimum fee for transactions
-const uint64_t MINIMUM_FEE                                   = 0;
+const size_t   CRYPTONOTE_DISPLAY_DECIMAL_POINT              = 9; // number of digits after decimal poin
+const uint64_t MINIMUM_FEE                                   = 0; // free transactions
 const uint64_t DEFAULT_DUST_THRESHOLD                        = MINIMUM_FEE;
 const uint64_t MAX_MIXIN                                     = 3;
-
 const uint64_t EXPECTED_NUMBER_OF_BLOCKS_PER_DAY             = 24 * 60 * 60 / DIFFICULTY_TARGET;
-//TODO There are options to tune CryptoNote's difficulty retargeting function.
-//TODO We recommend not to change it.
 const size_t   DIFFICULTY_WINDOW                             = 158; // blocks, N = int(45 * (600 / T) ^ 0.3)), N is the difficulty window, T is the target time
 const size_t   DIFFICULTY_CUT                                = 40;  // timestamps to cut after sorting (originally 60)
 const size_t   DIFFICULTY_LAG                                = 15;
 static_assert(2 * DIFFICULTY_CUT <= DIFFICULTY_WINDOW - 2, "Bad DIFFICULTY_WINDOW or DIFFICULTY_CUT");
-
 const size_t   MAX_BLOCK_SIZE_INITIAL                        = 2 * 1024 * 1024; // 2 mb
 const uint64_t MAX_BLOCK_SIZE_GROWTH_SPEED_NUMERATOR         = 100 * 1024;
 const uint64_t MAX_BLOCK_SIZE_GROWTH_SPEED_DENOMINATOR       = 365 * 24 * 60 * 60 / DIFFICULTY_TARGET;
-
 const uint64_t CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS     = 1;
 const uint64_t CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS    = DIFFICULTY_TARGET * CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS;
-
-const uint64_t CRYPTONOTE_MEMPOOL_TX_LIVETIME                = 60 * 5;     //seconds, 5 minutes
-const uint64_t CRYPTONOTE_MEMPOOL_TX_FROM_ALT_BLOCK_LIVETIME = 60 * 5;     //seconds, 5 minutes
+const uint64_t CRYPTONOTE_MEMPOOL_TX_LIVETIME                = 60 * 5; //seconds, 5 minutes
+const uint64_t CRYPTONOTE_MEMPOOL_TX_FROM_ALT_BLOCK_LIVETIME = 60 * 5; //seconds, 5 minutes
 const uint64_t CRYPTONOTE_NUMBER_OF_PERIODS_TO_FORGET_TX_DELETED_FROM_POOL = 1;  // CRYPTONOTE_NUMBER_OF_PERIODS_TO_FORGET_TX_DELETED_FROM_POOL * CRYPTONOTE_MEMPOOL_TX_LIVETIME = time to forget tx
-
 const size_t   FUSION_TX_MAX_SIZE                            = CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE * 30 / 100;
 const size_t   FUSION_TX_MIN_INPUT_COUNT                     = 12;
 const size_t   FUSION_TX_MIN_IN_OUT_COUNT_RATIO              = 4;
-
 const char     CRYPTONOTE_BLOCKS_FILENAME[]                  = "blocks.dat";
 const char     CRYPTONOTE_BLOCKINDEXES_FILENAME[]            = "blockindexes.dat";
 const char     CRYPTONOTE_BLOCKSCACHE_FILENAME[]             = "blockscache.dat";
@@ -76,24 +55,16 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 } // end namespace parameters
 
-//TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "cash2";
 const char     GENESIS_COINBASE_TX_HEX[]                     = "";
-
 const uint8_t  CURRENT_TRANSACTION_VERSION                   = 1;
-
 const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        = 10000;  //by default, blocks ids count in synchronizing
 const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            = 200;    //by default, blocks count in blocks downloading
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         = 1000;
-
-//TODO This port will be used by the daemon to establish connections with p2p network
 const int      P2P_DEFAULT_PORT                              = 12275;
-//TODO This port will be used by the daemon to interact with simlewallet
 const int      RPC_DEFAULT_PORT                              = 12276;
-
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                = 1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 = 5000;
-
 const size_t   P2P_CONNECTION_MAX_WRITE_BUFFER_SIZE          = 16 * 1024 * 1024; // 16 MB
 const uint32_t P2P_DEFAULT_CONNECTIONS_COUNT                 = 8;
 const size_t   P2P_DEFAULT_WHITELIST_CONNECTIONS_PERCENT     = 70;
@@ -106,11 +77,8 @@ const uint64_t P2P_DEFAULT_INVOKE_TIMEOUT                    = 60 * 2 * 1000; //
 const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          // 5 seconds
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "";
 
-//TODO Add here your network seed nodes
-const std::initializer_list<const char*> SEED_NODES = {
-  "",
-  "",
-};
+//seed nodes
+const std::initializer_list<const char*> SEED_NODES = {};
 
 struct CheckpointData {
   uint32_t height;
@@ -121,8 +89,6 @@ struct CheckpointData {
 __attribute__((unused))
 #endif
 
-// You may add here other checkpoints using the following format:
-// {<block height>, "<block hash>"},
 const std::initializer_list<CheckpointData> CHECKPOINTS = {};
 
 } // end namespace CryptoNote
--- tests/Basic/ScopeExit/main.cpp
+++ tests/Basic/ScopeExit/main.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2018 The Cash2 developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include <Common/ScopeExit.h>
 #include <gtest/gtest.h>
 #include <iostream>
