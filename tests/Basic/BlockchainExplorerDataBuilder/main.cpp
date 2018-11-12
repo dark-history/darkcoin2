41fee919f0158443d0bef103f062476c256a656a
aphivantrakul <>
2018-11-11 23:05:25 -0500

    Updated tests/Basic/BlockchainExplorerDataBuilder
    BlockchainExplorerDataBuilder now works correctly

--- tests/Basic/BlockchainExplorerDataBuilder/main.cpp
+++ tests/Basic/BlockchainExplorerDataBuilder/main.cpp
@@ -32,154 +32,42 @@ public
 
 // Helper Functions
 
-class TransactionValidator : public ITransactionValidator
-{
-  virtual bool checkTransactionInputs(const CryptoNote::Transaction& transaction, BlockInfo& maxUsedBlock) override
-  {
-    return true;
-  }
-
-  virtual bool checkTransactionInputs(const CryptoNote::Transaction& transaction, BlockInfo& maxUsedBlock, BlockInfo& lastFailed) override
-  {
-    return true;
-  }
-
-  virtual bool haveSpentKeyImages(const CryptoNote::Transaction& transaction) override
-  {
-    return false;
-  }
-
-  virtual bool checkTransactionSize(size_t blobSize) override
-  {
-    return true;
-  }
-};
-
-class TimeProvider : public ITimeProvider
-{
-public:
-  TimeProvider(time_t timeInput = time(nullptr)) : m_time(timeInput) {}; 
-  virtual time_t now() override { return m_time; };
-
-private:
-  time_t m_time;
-};
-
-size_t getBlockSize(const Transaction& baseTransaction, std::vector<Transaction> transactions)
-{
-  size_t blockSize = getObjectBinarySize(baseTransaction);
-  for (size_t i = 0; i < transactions.size(); ++i) {
-    blockSize += toBinaryArray(transactions[i]).size();
-  }
-
-  return blockSize;
-}
-
-size_t getMedianBlockSize(Blockchain& blockchain)
-{
-  size_t fromHeight = blockchain.getCurrentBlockchainHeight() - 1;
-  std::vector<size_t> blockSizes;
-  size_t maxCount = parameters::CRYPTONOTE_REWARD_BLOCKS_WINDOW;
-
-  blockchain.getBackwardBlocksSize(fromHeight, blockSizes, maxCount);
-
-  return Common::medianValue(blockSizes);
-}
 
 // Adds a new block to the blockchain
-bool addBlock1(Blockchain& blockchain, Currency& currency, tx_memory_pool& tx_memory_pool, Crypto::Hash& blockHash)
+bool addBlock1(core& core)
 {
-  uint32_t currentBlockchainHeight = blockchain.getCurrentBlockchainHeight();
+  uint32_t currentBlockchainHeight = core.get_current_blockchain_height();
 
-  // create block
+  AccountPublicAddress accountPublicAddress;
+  KeyPair viewKeyPair = generateKeyPair();
+  KeyPair spendKeyPair = generateKeyPair();
+  accountPublicAddress.viewPublicKey = viewKeyPair.publicKey;
+  accountPublicAddress.spendPublicKey = spendKeyPair.publicKey;
+
+  BinaryArray extraNonce;
+
   Block block;
-  block.nonce = 0;
-  block.timestamp = time(nullptr);
-  block.previousBlockHash = blockchain.getTailId();
+  difficulty_type difficulty;
+  uint32_t height;
 
-  std::vector<Transaction> transactions;
-
-  // create transaction
-  Transaction transaction;
-  transaction.version = CURRENT_TRANSACTION_VERSION;
-  // adding transaction extra so that each transaction is unique
-  transaction.extra = {1};
-  Crypto::PublicKey transactionPublicKey= generateKeyPair().publicKey;
-  for (int i = 0; i < 32; i++)
-  {
-    transaction.extra.push_back(transactionPublicKey.data[i]);
-  }
-
-  transactions.push_back(transaction);
-
-  Crypto::Hash transactionHash = getObjectHash(transaction);
-
-  // add transaction to block
-  block.transactionHashes.push_back(transactionHash);
-  
-  // add transaction to transaction mempool
-  tx_verification_context tvc;
-  bool keeped_by_block = true;
-  tx_memory_pool.add_tx(transaction, tvc, keeped_by_block);
-
-  // create coinbase transaction
-  block.baseTransaction = boost::value_initialized<Transaction>();
-
-  uint64_t alreadyGeneratedCoins;
-  Crypto::Hash lastBlockHash = blockchain.getTailId();
-  blockchain.getAlreadyGeneratedCoins(lastBlockHash, alreadyGeneratedCoins);
-
-  uint64_t fee = 0;
-  size_t medianBlockSize = getMedianBlockSize(blockchain);
-  size_t currentBlockSize = getBlockSize(block.baseTransaction, transactions);
-  size_t maxOuts = 1;
-
-  // Must use a while loop to figure out the current block size and coinbase reward
-  // Borrowed from TestGenerator.cpp
-  // The current block size is dependent on the amount of the coinbase reward
-  // BUT  
-  // The amount of the coinbase reward is dependent on the current block size
-  // Must use a while loop find the actual current block size so that the coinbase transaction output amount is correct
-  while (true)
-  {
-    currency.constructMinerTx(currentBlockchainHeight, medianBlockSize, alreadyGeneratedCoins, currentBlockSize,
-    fee, AccountPublicAddress(), block.baseTransaction, BinaryArray(), maxOuts);
-
-    size_t actualBlockSize = getBlockSize(block.baseTransaction, transactions);
-
-    if (actualBlockSize == currentBlockSize)
-    {
-      break;
-    }
-
-    currentBlockSize = actualBlockSize;
-  }
-
-  // add merkle root
-  block.merkleRoot = get_tx_tree_hash(block);
-
-  difficulty_type difficulty = blockchain.getDifficultyForNextBlock();
+  core.get_block_template(block, accountPublicAddress, difficulty, height, extraNonce);
 
   // find nonce appropriate for current difficulty
   Crypto::Hash proofOfWorkIgnore = NULL_HASH;
   Crypto::cn_context context;
-  while(!currency.checkProofOfWork(context, block, difficulty, proofOfWorkIgnore))
+  while(!core.currency().checkProofOfWork(context, block, difficulty, proofOfWorkIgnore))
   {
     block.nonce++;
   }
 
-  // add block to blockchain
-  block_verification_context bvc;
-  bool added = blockchain.addNewBlock(block, bvc);
+  bool blockAdded = core.handle_block_found(block);
 
-  blockHash = get_block_hash(block);
-
-  return added;
+  return blockAdded;
 }
 
 // Adds an empty block to the blockchain
 // returns transaction public key of the coinbase transaction
-bool addBlock5(Blockchain& blockchain, Currency& currency, core& core, const AccountPublicAddress& minerPublicAddress, Crypto::PublicKey& transactionPublicKey, Crypto::Hash& transactionHash)
+bool addBlock5(core& core, const AccountPublicAddress& minerPublicAddress, Crypto::PublicKey& transactionPublicKey, Crypto::Hash& transactionHash)
 {
   uint32_t currentBlockchainHeight = core.get_current_blockchain_height();
 
@@ -195,13 +83,10 @@ bool addBlock5(Blockchain& blockchain, Currency& currency, core& core, const Acc
 
   transactionHash = getObjectHash(block.baseTransaction);
 
-  // add merkle root
-  block.merkleRoot = get_tx_tree_hash(block);
-
   // find nonce appropriate for current difficulty
   Crypto::Hash proofOfWorkIgnore;
   Crypto::cn_context context;
-  while(!currency.checkProofOfWork(context, block, difficulty, proofOfWorkIgnore))
+  while(!core.currency().checkProofOfWork(context, block, difficulty, proofOfWorkIgnore))
   {
     block.nonce++;
   }
@@ -213,7 +98,7 @@ bool addBlock5(Blockchain& blockchain, Currency& currency, core& core, const Acc
 
 // Adds an empty block to the blockchain
 // return block that was added
-bool addBlock8(core& core, Currency& currency, Block& block)
+bool addBlock8(core& core, Block& block)
 {
   uint32_t currentBlockchainHeight = core.get_current_blockchain_height();
 
@@ -238,7 +123,7 @@ bool addBlock8(core& core, Currency& currency, Block& block)
   // find nonce appropriate for current difficulty
   Crypto::Hash proofOfWorkIgnore;
   Crypto::cn_context context;
-  while(!currency.checkProofOfWork(context, block, difficulty, proofOfWorkIgnore))
+  while(!core.currency().checkProofOfWork(context, block, difficulty, proofOfWorkIgnore))
   {
     block.nonce++;
   }
@@ -250,11 +135,11 @@ bool addBlock8(core& core, Currency& currency, Block& block)
 
 // creates a transaction and adds it to the mempool
 // returns transaction created
-bool createTransaction4(core& core, Blockchain& blockchain, const AccountKeys& senderAccountKeys, const AccountPublicAddress& receiverAccountPublicAddress, Crypto::PublicKey inputTransactionPublicKey, Crypto::Hash inputTransactionHash, Transaction& transaction)
+bool createTransaction4(core& core, const AccountKeys& senderAccountKeys, const AccountPublicAddress& receiverAccountPublicAddress, Crypto::PublicKey inputTransactionPublicKey, Crypto::Hash inputTransactionHash, Transaction& transaction)
 {
   // create transaction
   transaction.version = CURRENT_TRANSACTION_VERSION;
-  transaction.unlockTime = blockchain.getCurrentBlockchainHeight() + parameters::CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW;
+  transaction.unlockTime = core.get_current_blockchain_height() + parameters::CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW;
 
   // create transaction random transaction key
   KeyPair transactionKeyPair = generateKeyPair();
@@ -275,16 +160,15 @@ bool createTransaction4(core& core, Blockchain& blockchain, const AccountKeys& s
 
   // amount = 894069565
   //                     5
-  //                    /  6
-  //                   /  /  50
-  //                  /  /  /  900
-  //                 /  /  /  /  6000
-  //                /  /  /  /  /  400000
-  //               /  /  /  /  /  /  9000000
-  //              /  /  /  /  /  /  /  80000000
+  //                    /  60
+  //                   /  /  500
+  //                  /  /  /  9000
+  //                 /  /  /  /  60000
+  //                /  /  /  /  /  4000000
+  //               /  /  /  /  /  /  90000000
+  //              /  /  /  /  /  /  /  800000000
   //             /  /  /  /  /  /  /  /
   // indexes = {0, 0, 0, 1, 1, 1, 1, 1} 
-
 
   // create key image
   KeyPair inputStealthKeyPair;
@@ -405,20 +289,17 @@ TEST(BlockchainExplorerDataBuilder, 2)
   bool loadExisting = false;
   ASSERT_TRUE(core.init(coreConfig, minerConfig, loadExisting));
 
-  // set genesis block of core
-  Block genesisBlock = currency.genesisBlock();
-  ASSERT_TRUE(core.set_genesis_block(genesisBlock));
-
   // create BlockchainExplorerDataBuilder object
   CryptoNoteProtocolQuery protocol;
   BlockchainExplorerDataBuilder blockchainExplorerDataBuilder(core, protocol);
 
   BlockDetails blockDetails;
+  Block genesisBlock = currency.genesisBlock();
   ASSERT_TRUE(blockchainExplorerDataBuilder.fillBlockDetails(genesisBlock, blockDetails));
 
   // add new block to blockchain
   Block block;
-  ASSERT_TRUE(addBlock8(core, currency, block));
+  ASSERT_TRUE(addBlock8(core, block));
 
   ASSERT_TRUE(blockchainExplorerDataBuilder.fillBlockDetails(block, blockDetails));
 }
@@ -429,17 +310,8 @@ TEST(BlockchainExplorerDataBuilder, 3)
   // create core object
   Logging::ConsoleLogger logger;
   Currency currency = CurrencyBuilder(logger).currency();
-  TransactionValidator validator;
-  TimeProvider timeProvider;
-  tx_memory_pool tx_memory_pool(currency, validator, timeProvider, logger);
   CryptonoteProtocol crpytonoteProtocol;
   
-  Blockchain blockchain(currency, tx_memory_pool, logger);
-
-  // initialize blockchain
-  std::string config_folder = Tools::getDefaultDataDirectory();
-  ASSERT_TRUE(blockchain.init(config_folder, false));
-
   core core(currency, &crpytonoteProtocol, logger);
 
   // initialize core object
@@ -447,14 +319,6 @@ TEST(BlockchainExplorerDataBuilder, 3)
   MinerConfig minerConfig;
   bool loadExisting = false;
   ASSERT_TRUE(core.init(coreConfig, minerConfig, loadExisting));
-
-  // set genesis block of core
-  Block genesisBlock = currency.genesisBlock();
-  ASSERT_TRUE(core.set_genesis_block(genesisBlock));
-
-  Crypto::Hash blockHashIgnore;
-
-  ASSERT_TRUE(addBlock1(blockchain, currency, tx_memory_pool, blockHashIgnore));
 
   // miner finds a block
   AccountKeys minerAccountKeys;
@@ -469,12 +333,12 @@ TEST(BlockchainExplorerDataBuilder, 3)
   Crypto::Hash transactionHash;
 
   // add block that sends the coinbase transaction to the miner's address
-  ASSERT_TRUE(addBlock5(blockchain, currency, core, minerAccountKeys.address, transactionPublicKey, transactionHash));
+  ASSERT_TRUE(addBlock5(core, minerAccountKeys.address, transactionPublicKey, transactionHash));
 
   // allow the miner's coinbase transaction to mature for 60 blocks
   for (int i = 0; i < parameters::CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW; i++)
   {
-    ASSERT_TRUE(addBlock1(blockchain, currency, tx_memory_pool, blockHashIgnore));
+    ASSERT_TRUE(addBlock1(core));
   }
 
   // create transaction to send miner's money to another person
@@ -489,10 +353,10 @@ TEST(BlockchainExplorerDataBuilder, 3)
   // create a transaction and add it to the mempool
   Crypto::Hash inputTransactionHash = transactionHash;
   Transaction transaction;
-  ASSERT_TRUE(createTransaction4(core, blockchain, senderAccountKeys, receiverAccountPublicAddress, transactionPublicKey, inputTransactionHash, transaction));
+  ASSERT_TRUE(createTransaction4(core, senderAccountKeys, receiverAccountPublicAddress, transactionPublicKey, inputTransactionHash, transaction));
 
   // add transaction to a block
-  ASSERT_TRUE(addBlock1(blockchain, currency, tx_memory_pool, blockHashIgnore));
+  ASSERT_TRUE(addBlock1(core));
 
   // create BlockchainExplorerDataBuilder object
   CryptoNoteProtocolQuery protocol;
