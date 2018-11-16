a692b2dfb64ed90e028494b559a51c2944886fc5
aphivantrakul <>
2018-11-15 22:35:31 -0500

    Updated generateGenesisBlock() in Currency.cpp
    Updated the genesis block message
    Revert genesis block timestamp back to 0. time(nullptr) gets a new each time causing the genesis block id to be different between nodes.
    
    Added basic test for addresses

---
+++ tests/Basic/Address/crypto-tests.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Copyright (c) 2018 The Cash2 developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+#if defined(__cplusplus)
+#include "crypto/crypto.h"
+
+extern "C" {
+#endif
+
+void setup_random(void);
+
+#if defined(__cplusplus)
+}
+
+bool check_scalar(const Crypto::SecretKey &secretKey);
+
+#endif
---
+++ tests/Basic/Address/crypto.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Copyright (c) 2018 The Cash2 developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "crypto/crypto.cpp"
+
+#include "crypto-tests.h"
+
+bool check_scalar(const Crypto::SecretKey &secretKey) {
+  return Crypto::sc_check(reinterpret_cast<const unsigned char*>(&secretKey)) == 0;
+}
