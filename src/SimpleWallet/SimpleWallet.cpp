9d6d05be58e1a379bf80ce02fc435a0d7b1aac35
aphivantrakul <aphivantrakul@gmail.com>
2019-11-16 12:41:00 -0500

    SimpleWallet JSON RPC : Disabled RPC for SimpleWallet
    Reason for changes : SimpleWallet should be as simple to use as possible for new users. If users want to use a wallet with RPC functionality, they should use Walletd.
    
    This commit is a duplicate of commits 29959d5a8397cdd3ecde72a54ebfaeb6e830e2f9 and 7a86d926ef234ef917e97225ca57e103c864d70c from the master branch

--- src/SimpleWallet/SimpleWallet.cpp
+++ src/SimpleWallet/SimpleWallet.cpp
@@ -32,7 +32,6 @@
 #include "Rpc/CoreRpcServerCommandsDefinitions.h"
 #include "Rpc/HttpClient.h"
 
-#include "Wallet/WalletRpcServer.h"
 #include "WalletLegacy/WalletLegacy.h"
 #include "Wallet/LegacyKeysImporter.h"
 #include "WalletLegacy/WalletHelper.h"
@@ -1393,7 +1392,6 @@ int main(int argc, char* argv[]) {
   command_line::add_arg(desc_params, arg_command);
   command_line::add_arg(desc_params, arg_log_level);
   command_line::add_arg(desc_params, arg_testnet);
-  Tools::wallet_rpc_server::init_options(desc_params);
 
   po::positional_options_description positional_options;
   positional_options.add(arg_command.name, -1);
@@ -1446,114 +1444,31 @@ int main(int argc, char* argv[]) {
   CryptoNote::Currency currency = CryptoNote::CurrencyBuilder(logManager).
     testnet(command_line::get_arg(vm, arg_testnet)).currency();
 
-  if (command_line::has_arg(vm, Tools::wallet_rpc_server::arg_rpc_bind_port)) {
-    //runs wallet with rpc interface
-    if (!command_line::has_arg(vm, arg_wallet_file)) {
-      logger(ERROR, BRIGHT_RED) << "Wallet file not set.";
-      return 1;
-    }
-
-    if (!command_line::has_arg(vm, arg_daemon_address)) {
-      logger(ERROR, BRIGHT_RED) << "Daemon address not set.";
-      return 1;
-    }
-
-    if (!command_line::has_arg(vm, arg_password)) {
-      logger(ERROR, BRIGHT_RED) << "Wallet password not set.";
-      return 1;
-    }
-
-    std::string wallet_file = command_line::get_arg(vm, arg_wallet_file);
-    std::string wallet_password = command_line::get_arg(vm, arg_password);
-    std::string daemon_address = command_line::get_arg(vm, arg_daemon_address);
-    std::string daemon_host = command_line::get_arg(vm, arg_daemon_host);
-    uint16_t daemon_port = command_line::get_arg(vm, arg_daemon_port);
-    if (daemon_host.empty())
-      daemon_host = "localhost";
-    if (!daemon_port)
-      daemon_port = RPC_DEFAULT_PORT;
-
-    if (!daemon_address.empty()) {
-      if (!parseUrlAddress(daemon_address, daemon_host, daemon_port)) {
-        logger(ERROR, BRIGHT_RED) << "failed to parse daemon address: " << daemon_address;
-        return 1;
-      }
-    }
-
-    std::unique_ptr<INode> node(new NodeRpcProxy(daemon_host, daemon_port));
-
-    std::promise<std::error_code> errorPromise;
-    std::future<std::error_code> error = errorPromise.get_future();
-    auto callback = [&errorPromise](std::error_code e) {errorPromise.set_value(e); };
-    node->init(callback);
-    if (error.get()) {
-      logger(ERROR, BRIGHT_RED) << ("failed to init NodeRPCProxy");
-      return 1;
-    }
-
-    std::unique_ptr<IWalletLegacy> wallet(new WalletLegacy(currency, *node.get()));
-
-    std::string walletFileName;
-    try  {
-      walletFileName = ::tryToOpenWalletOrLoadKeysOrThrow(logger, wallet, wallet_file, wallet_password);
-
-      logger(INFO) << "available balance: " << currency.formatAmount(wallet->actualBalance()) <<
-      ", locked amount: " << currency.formatAmount(wallet->pendingBalance());
-
-      logger(INFO, BRIGHT_GREEN) << "Loaded ok";
-    } catch (const std::exception& e)  {
-      logger(ERROR, BRIGHT_RED) << "Wallet initialize failed: " << e.what();
-      return 1;
-    }
-
-    Tools::wallet_rpc_server wrpc(dispatcher, logManager, *wallet, *node, currency, walletFileName);
-
-    if (!wrpc.init(vm)) {
-      logger(ERROR, BRIGHT_RED) << "Failed to initialize wallet rpc server";
-      return 1;
-    }
-
-    Tools::SignalHandler::install([&wrpc, &wallet] {
-      wrpc.send_stop_signal();
-    });
-
-    logger(INFO) << "Starting wallet rpc server";
-    wrpc.run();
-    logger(INFO) << "Stopped wallet rpc server";
-    
-    try {
-      logger(INFO) << "Storing wallet...";
-      CryptoNote::WalletHelper::storeWallet(*wallet, walletFileName);
-      logger(INFO, BRIGHT_GREEN) << "Stored ok";
-    } catch (const std::exception& e) {
-      logger(ERROR, BRIGHT_RED) << "Failed to store wallet: " << e.what();
-      return 1;
-    }
-  } else {
-    //runs wallet with console interface
-    CryptoNote::simple_wallet wal(dispatcher, currency, logManager);
-    
-    if (!wal.init(vm)) {
-      logger(ERROR, BRIGHT_RED) << "Failed to initialize wallet"; 
-      return 1; 
-    }
-
-    std::vector<std::string> command = command_line::get_arg(vm, arg_command);
-    if (!command.empty())
-      wal.process_command(command);
-
-    Tools::SignalHandler::install([&wal] {
-      wal.stop();
-    });
-    
-    wal.run();
-
-    if (!wal.deinit()) {
-      logger(ERROR, BRIGHT_RED) << "Failed to close wallet";
-    } else {
-      logger(INFO) << "Wallet closed";
-    }
+  
+  //runs wallet with console interface
+  CryptoNote::simple_wallet wal(dispatcher, currency, logManager);
+  
+  if (!wal.init(vm)) {
+    logger(ERROR, BRIGHT_RED) << "Failed to initialize wallet"; 
+    return 1; 
   }
+
+  std::vector<std::string> command = command_line::get_arg(vm, arg_command);
+  if (!command.empty())
+    wal.process_command(command);
+
+  Tools::SignalHandler::install([&wal] {
+    wal.stop();
+  });
+  
+  wal.run();
+
+  if (!wal.deinit()) {
+    logger(ERROR, BRIGHT_RED) << "Failed to close wallet";
+  } else {
+    logger(INFO) << "Wallet closed";
+  }
+
   return 1;
   //CATCH_ENTRY_L0("main", 1);
 }
