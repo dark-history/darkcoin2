cca1f65b5bb7342887d502960c6ec48985286df1
aphivantrakul <aphivantrakul@gmail.com>
2021-01-03 11:51:19 +0700

    Copyright - Update year to 2021 for Cash2 developers

--- src/Transfers/TransfersConsumer.cpp
+++ src/Transfers/TransfersConsumer.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2011-2016 The Cryptonote developers, The Bytecoin developers
 // Copyright (c) 2018, The BBSCoin Developers
 // Copyright (c) 2018, The Karbo Developers
-// Copyright (c) 2018-2020 The Cash2 developers
+// Copyright (c) 2018-2021 The Cash2 developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
